7a2db28d8a299d77e279040a0dde4b13bfee3ea3
General1717 <31494187+General1717@users.noreply.github.com>
2018-03-09 18:19:39 -0600

    exercise 1
    im stuck but looking for the solution

--- Exercise#1/ExtractURLs.py
+++ Exercise#1/ExtractURLs.py
@@ -1,2 +1,21 @@
 # Write your code below
 # You will need to pip-install the modules(libraries) you'll need (See Useful Libraries in the readme.md)
+
+import requests
+from bs4 import BeautifulSoup
+
+def my_spider(max_pages):
+    page = 1
+    while page <= max_pages:
+        site = 'https://www.xkcd.com' + str(page)
+        web_code = requests.get(site)
+        plain_text = web_code.text
+        soup = BeautifulSoup(plain_text)
+        for link in soup.findAll('a', {'class': 'http'}):
+            href = link.get('href')
+            print(href)
+        page += 1
+
+
+my_spider(1)
+            
